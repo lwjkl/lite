a579c744ef60eed4c557d4de1c2c9e6019e0afd3
wjlee <lwjkl@icloud.com>
2025-07-22 09:19:22 +0800

    replace resnet with dino

--- utils.py
+++ utils.py
@@ -1,31 +1,6 @@
 import numpy
 import torch
-import torchvision
 import torchvision.transforms.v2 as T
-
-
-class ResNet50FeatureExtractor(torch.nn.Module):
-    def __init__(self):
-        super(ResNet50FeatureExtractor, self).__init__()
-        self.resnet = torch.nn.Sequential(
-            *list(
-                torchvision.models.resnet50(
-                    weights=torchvision.models.ResNet50_Weights.IMAGENET1K_V2
-                ).children()
-            )[:-1]
-        )
-
-    def forward(self, x):
-        x = self.resnet(x)
-        x = torch.flatten(x, 1)
-        return x
-
-
-def get_embeder(device="cpu"):
-    model = ResNet50FeatureExtractor()
-    model = model.to(device)
-    model.eval()
-    return model
 
 
 def embed(
@@ -35,7 +10,7 @@ def embed(
         [
             T.Resize(size=(224, 224)),
             T.ToDtype(torch.float32, scale=True),
-            T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
         ]
     )
     image = torch.from_numpy(image).permute(2, 0, 1)
@@ -47,3 +22,10 @@ def embed(
     embedding = logit.cpu().numpy()
 
     return embedding[0]
+
+
+def get_embeder(device="cpu"):
+    model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
+    model = model.to(device)
+    model.eval()
+    return model
